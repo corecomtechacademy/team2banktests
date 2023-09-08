9f6659b6261a8ee378df5e964b3bdb8d3243c55d
Aidan Comer <alhcomer@hotmail.co.uk>
2023-09-08 14:39:08 +0100

    added form tests

--- src/main/java/com/cta/loanapp/tests/pages/LoanApplicationPage.java
+++ src/main/java/com/cta/loanapp/tests/pages/LoanApplicationPage.java
@@ -4,10 +4,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+import java.util.Map;
+
 public class LoanApplicationPage extends BasePage {
     public LoanApplicationPage(WebDriver driver) {
         super(driver);
     }
+
 
     public String extractAdminAccessDeniedText() {
         WebElement adminAccessDeniedDiv = wait.until(
@@ -21,5 +25,23 @@ public class LoanApplicationPage extends BasePage {
                 d -> driver.findElement(By.cssSelector("div.container h1"))
         );
         return loanApplicationTitleText.getText();
+    }
+
+    public void submitApplicationDetails(Map<String, Object> dataForInputs) {
+        List<WebElement> formInputs = wait.until(d -> driver.findElements(By.tagName("input")));
+        for (WebElement input : formInputs) {
+            for (Map.Entry<String, Object> entry : dataForInputs.entrySet()) {
+                if (input.getAttribute("name").equals(entry.getKey())) {
+                    input.sendKeys(entry.getValue().toString());
+                }
+            }
+        }
+        WebElement submitBtn = driver.findElement(By.cssSelector("button[type='submit']"));
+        submitBtn.click();
+    }
+
+    public String getSuccessfulApplicationResultTitle() {
+        WebElement result = driver.findElement(By.cssSelector("div.container h1"));
+        return result.getText();
     }
 }
